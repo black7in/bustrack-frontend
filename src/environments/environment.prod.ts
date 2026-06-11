917c3446a67d36ad283f01547d463f0371a91ac5
Black7in <elperroblack158@gmail.com>
2026-06-10 23:17:09 -0400

    Prod URLs: MS-Core y MS-Operaciones

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ms-core.tu-dominio.com',
-  operacionesApiUrl: 'https://ms-operaciones.tu-dominio.com',
+  apiUrl: 'http://3.22.171.236:3000',
+  operacionesApiUrl: 'http://34.10.181.185:8001',
 };
