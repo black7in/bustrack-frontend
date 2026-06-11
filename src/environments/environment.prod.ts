e35d212fbc21ce0d50c18537d37d9aff5dd1cd3d
Black7in <elperroblack158@gmail.com>
2026-06-11 00:13:05 -0400

    CORS fix: nginx reverse proxy + same-origin URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://3.22.171.236:3000',
-  operacionesApiUrl: 'http://34.10.181.185:8001',
-  iaApiUrl: 'http://34.30.80.191:8002',
+  apiUrl: '',
+  operacionesApiUrl: '/api/operaciones',
+  iaApiUrl: '/api/ia',
 };
